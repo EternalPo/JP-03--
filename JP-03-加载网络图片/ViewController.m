3ab71227bf20ccdb8c2386a6902df6650efd68d7
EternaPo <guopo916@icloud.com>
2015-01-10 21:59:50 +0800

    实现数据源方法，返回cell，基本设置完成

--- JP-03-加载网络图片/ViewController.m
+++ JP-03-加载网络图片/ViewController.m
@@ -7,13 +7,41 @@
 //
 
 #import "ViewController.h"
+#import "JPAppInfo.h"
 
 @interface ViewController ()
-
+/**
+ *  模型数组
+ */
+@property (nonatomic, strong) NSArray *appInfos;
 @end
 
 @implementation ViewController
 
+/**
+ *  懒加载数组
+ */
+- (NSArray *)appInfos
+{
+    if (_appInfos== nil) {
+        // 读取plist
+        NSURL *url = [[NSBundle mainBundle] URLForResource:@"apps.plist" withExtension:nil];
+        
+        NSArray *dictA = [NSArray arrayWithContentsOfURL:url];
+        NSMutableArray *arrayM= [NSMutableArray arrayWithCapacity:dictA.count];
+        // 遍历数组中的每个字典，转模型
+        // 用代码块比forin 效率更高
+        [dictA enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            // 字典转模型
+            JPAppInfo *appInfo = [JPAppInfo appInfoWithDict:obj];
+            // 添加模型到数组
+            [arrayM addObject:appInfo];
+        }];
+        _appInfos = [arrayM copy];
+
+    }
+    return _appInfos;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -24,4 +52,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - 数据源方法
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.appInfos.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    // 创建cell
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"appCell"];
+    
+    JPAppInfo *app = self.appInfos[indexPath.row];
+    
+    cell.textLabel.text = app.name;
+    
+    cell.detailTextLabel.text = app.download;
+    
+    return cell;
+    
+}
 @end
